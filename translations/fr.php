991f5ceec312649993de6c75d2933e0c67139973
Matt75 <5262628+Matt75@users.noreply.github.com>
2020-04-01 14:43:13 +0200

    Add French translations

---
+++ translations/fr.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 2007-2020 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+global $_MODULE;
+$_MODULE = [];
+$_MODULE['<{pscartbanner}prestashop>pscartbanner_8474ffa34c1da57b44fe49cb385cd8ca'] = 'Bannière sur la page panier';
+$_MODULE['<{pscartbanner}prestashop>pscartbanner_96df58db9bec7cfa22273ae2f10694c2'] = 'Affiche une bannière personnalisable sur la page panier. ';
+$_MODULE['<{pscartbanner}prestashop>adminpscartbannercontroller_a3fcbca4ae66e2e8faad2ade91962215'] = 'Contenu';
+$_MODULE['<{pscartbanner}prestashop>adminpscartbannercontroller_447efb99e205a877cbaf985ce87a01f4'] = 'Couleur de la bordure';
+$_MODULE['<{pscartbanner}prestashop>adminpscartbannercontroller_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
